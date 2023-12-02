fe03a1cd3f34f5993e4a0d900cf4630ec8811401
Bravo <djdram@ya.ru>
2023-12-02 23:57:29 +0300

    Update Drastic 1.7
    Update Drastic new version

--- static/packages/Emu/Nintendo - DS (Drastic)/Emu/NDS/launch.sh
+++ static/packages/Emu/Nintendo - DS (Drastic)/Emu/NDS/launch.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
+CUST_LOGO=0
+CUST_CPUCLOCK=1
+USE_752x560_RES=0
+
 mydir=`dirname "$0"`
+
+purge_devil() {
+    if pgrep -f "/dev/l" > /dev/null; then
+        echo "Process /dev/l is running. Killing it now..."
+        killall -9 l
+    else
+        echo "Process /dev/l is not running."
+    fi
+}
+
 cd $mydir
+if [ ! -f "/tmp/.show_hotkeys" ]; then
+    touch /tmp/.show_hotkeys
+    LD_LIBRARY_PATH=./libs:/customer/lib:/config/lib ./show_hotkeys
+fi
 
 export HOME=$mydir
 export PATH=$mydir:$PATH
@@ -9,39 +27,36 @@ export SDL_VIDEODRIVER=mmiyoo
 export SDL_AUDIODRIVER=mmiyoo
 export EGL_VIDEODRIVER=mmiyoo
 
-. /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
-
-CUST_LOGO=0
-CUST_CPUCLOCK=1
-
-
-# Uncomment this section if you need to customize the Drastic menu logo :
-# if [ "$CUST_LOGO" == "1" ]; then
-    # echo "convert resources/logo/0.png to drastic_logo_0.raw"
-    # echo "convert resources/logo/1.png to drastic_logo_1.raw"
-    # ./png2raw
-# fi
-
-resolution=$(head -n 1 /tmp/screen_resolution)
-if [ "$resolution" = "752x560" ]; then
-    USE_752x560_RES=1
+if [ -f /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh ]; then
+    /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
 else
-    USE_752x560_RES=0
+    killall audioserver
+    killall audioserver.mod
+fi
+#purge_devil
+
+if [ "$USE_752x560_RES" == "1" ]; then
+    fbset -g 752 560 752 1120 32
+    fbset > fbset.log
 fi
 
+cd $mydir
+if [ "$CUST_LOGO" == "1" ]; then
+    ./png2raw
+fi
 
 sv=`cat /proc/sys/vm/swappiness`
 
-echo 10 > /proc/sys/vm/swappiness  # 60 by default
+# 60 by default
+echo 10 > /proc/sys/vm/swappiness
 
 cd $mydir
 
 if [ "$CUST_CPUCLOCK" == "1" ]; then
-    cpuclock 1500
+    ./cpuclock 1500
 fi
 
 ./drastic "$1"
-
 sync
 
 echo $sv > /proc/sys/vm/swappiness
