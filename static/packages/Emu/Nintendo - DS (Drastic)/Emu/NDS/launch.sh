f7dd70d5ca21d773ba165329d29df7f4eb85c72f
Bravo <djdram@ya.ru>
2023-12-28 09:11:44 -0500

    Better Drastic bios requirement detection
    Drastic now check if original bios is required even if Onion is reinstalled.
    It ensures compatibility the save state compatibility even after a fresh installation of Onion.

--- static/packages/Emu/Nintendo - DS (Drastic)/Emu/NDS/launch.sh
+++ static/packages/Emu/Nintendo - DS (Drastic)/Emu/NDS/launch.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
-CUST_LOGO=0
-CUST_CPUCLOCK=1
-USE_752x560_RES=0
-
-mydir=`dirname "$0"`
-
-purge_devil() {
-    if pgrep -f "/dev/l" > /dev/null; then
-        echo "Process /dev/l is running. Killing it now..."
-        killall -9 l
-    else
-        echo "Process /dev/l is not running."
-    fi
-}
-
+mydir=$(dirname "$0")
 cd $mydir
-if [ ! -f "/tmp/.show_hotkeys" ]; then
-    touch /tmp/.show_hotkeys
-    LD_LIBRARY_PATH=./libs:/customer/lib:/config/lib ./show_hotkeys
+
+if [ -e "/mnt/SDCARD/Saves/CurrentProfile/states/Drastic/.originalBiosRequired" ]; then
+    if [ ! -e "/mnt/SDCARD/Emu/NDS/system/nds_bios_arm7.bin" ] || [ ! -e "/mnt/SDCARD/Emu/NDS/system/nds_bios_arm9.bin" ] || [ ! -e "/mnt/SDCARD/Emu/NDS/system/nds_firmware.bin" ]; then
+        infoPanel --title "NDS Bios Required" --message "Your save states require official NDS bios to work.\nDrastic emulator will not run until you add\nthe following files in  'Emu/NDS/system':\nnds_bios_arm7.bin\nnds_bios_arm9.bin\nnds_firmware.bin"
+        exit
+    fi
 fi
 
 export HOME=$mydir
@@ -27,39 +16,40 @@ export SDL_VIDEODRIVER=mmiyoo
 export SDL_AUDIODRIVER=mmiyoo
 export EGL_VIDEODRIVER=mmiyoo
 
-if [ -f /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh ]; then
-    /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
+. /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
+
+CUST_LOGO=0
+CUST_CPUCLOCK=1
+
+# Uncomment this section if you need to customize the Drastic menu logo :
+# if [ "$CUST_LOGO" == "1" ]; then
+# echo "convert resources/logo/0.png to drastic_logo_0.raw"
+# echo "convert resources/logo/1.png to drastic_logo_1.raw"
+# ./png2raw
+# fi
+
+resolution=$(head -n 1 /tmp/screen_resolution)
+if [ "$resolution" = "752x560" ]; then
+    USE_752x560_RES=1
 else
-    killall audioserver
-    killall audioserver.mod
-fi
-#purge_devil
-
-if [ "$USE_752x560_RES" == "1" ]; then
-    fbset -g 752 560 752 1120 32
-    fbset > fbset.log
+    USE_752x560_RES=0
 fi
 
-cd $mydir
-if [ "$CUST_LOGO" == "1" ]; then
-    ./png2raw
-fi
+sv=$(cat /proc/sys/vm/swappiness)
 
-sv=`cat /proc/sys/vm/swappiness`
-
-# 60 by default
-echo 10 > /proc/sys/vm/swappiness
+echo 10 >/proc/sys/vm/swappiness # 60 by default
 
 cd $mydir
 
 if [ "$CUST_CPUCLOCK" == "1" ]; then
-    ./cpuclock 1500
+    cpuclock 1500
 fi
 
 ./drastic "$1"
+
 sync
 
-echo $sv > /proc/sys/vm/swappiness
+echo $sv >/proc/sys/vm/swappiness
 
 if [ "$USE_752x560_RES" == "1" ]; then
     fbset -g 640 480 640 960 32
