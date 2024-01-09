5ec8f60a4de004a110fe59674db70f567e4a00b3
Bravo <djdram@ya.ru>
2024-01-09 04:59:26 -0500

    Fix hint no screenshoot images after back from first image
    Exit from gallery when existing images will return code 255, that cause
    a error hint, but do not need it actually.

--- static/packages/App/Gallery (Screenshot viewer)/App/Screenshots_Viewer/launch.sh
+++ static/packages/App/Gallery (Screenshot viewer)/App/Screenshots_Viewer/launch.sh
@@ -4,6 +4,9 @@ echo ":: Галерея - Просмотр изображений ::"
 /mnt/SDCARD/.tmp_update/bin/infoPanel -d /mnt/SDCARD/Screenshots --show-theme-controls
 ec=$?
 
-if [ $ec -ne 0 ]; then
-    /mnt/SDCARD/.tmp_update/bin/infoPanel -t Галерея -m "Изображения не найдены"
+# cancel or success from infoPanel
+if [ $ec -eq 255 ] || [ $ec -eq 0 ] ; then
+    exit 0
 fi
+
+/mnt/SDCARD/.tmp_update/bin/infoPanel -t Галерея -m "Изображения не найдены"
